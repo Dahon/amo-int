e4d796e52d566cc6fb3db2c4f98014d54bedd8fa
dahon <yertaiev@gmail.com>
2022-04-13 15:08:13 +0300

    init

--- app/Http/Controllers/components/AmoTypeConstants.php
+++ app/Http/Controllers/components/AmoTypeConstants.php
@@ -12,8 +12,8 @@
 
 
       public const AUTOSTART_PIPELINE_ID = 5301544;
-      public const AUTOSTART_PENDING = 47135884;
-      public const AUTOSTART_APPROVED_ID = 47135872;
-      public const AUTOSTART_ALTER = 47135875;
-      public const AUTOSTART_DECLINED = 47135878;
+      public const AUTOSTART_PENDING = 47238265;
+      public const AUTOSTART_APPROVED_ID = 47238268;
+      public const AUTOSTART_ALTER = 47238319;
+      public const AUTOSTART_DECLINED = 47238322;
   }
