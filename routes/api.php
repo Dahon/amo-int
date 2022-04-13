4c3d561614b900e27b6848bece553239fcf9bcd0
dahon <yertaiev@gmail.com>
2022-04-13 20:47:44 +0300

    init

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Log;
     use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AmoIntController;
 
 /*
 |--------------------------------------------------------------------------
